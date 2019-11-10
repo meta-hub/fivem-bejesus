64870a77335067d453bb6a8d52f92384a0731d78
meta-hub <56391287+meta-hub@users.noreply.github.com>
2019-11-10 11:18:29 +1030

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,4 @@
 jesus = {}
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj; end)
-Citizen.CreateThread(function() while not ESX do TriggerEvent('esx:getSharedObject', function(obj) ESX = obj; end); Wait(0); end; end)
 
 jesus.controls = {
   ["Enable"] = 47,
